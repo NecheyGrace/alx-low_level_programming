185c74332be6f40bd214dcaf659efcdc78fc6195
Amara Metu <nechemetu@gmail.com>
2022-03-11 14:36:58 -0800

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
-int main(void)                            /* Most important part of the program!  */
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    int n;                          /* Need a variable... */
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    printf( "Please enter a random number: " );  /* Asks for age */
-    scanf( "%d", &n );                 /* The input is put in age */
-    if ( n > 0 ) {                  /* If the age is less than 100 */
-        printf ("is positive\n" ); /* Just to show you it works... */
-    }
-    else if ( n == 0 ) {            /* I use else just to show an example */
-        printf( "is zero\n" );
-    }
-    else if ( n < 0 ) {            /* I use else just to show an example */
-        printf( "is negative\n" );
-    }
+	int n;
 
-    else {
-        printf( "You are really old\n" );     /* Executed if no other statement is */
-    }
-  return (0);
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }

f08debfa0f3fb78a957c3de493a11efc78bec264
Amara Metu <nechemetu@gmail.com>
2022-04-09 01:00:04 +0100

    A program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,32 +1,29 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
+
 /**
  * main - multiplies two positive numbers
  * @argc: n arguments
  * @argv: args
  * Return: int
  */
-void _is_zero(char *argv[])
+int main(int argc, char *argv[])
 {
-	int i, isn1 = 1, isn2 = 1;
-
-	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
-	if (isn1 == 1 || isn2 == 1)
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-		exit(0);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
 	}
+	mul = atol(argv[1]) *atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
